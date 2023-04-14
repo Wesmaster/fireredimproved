530f9343b5bb6111b2f57cd1467210acd9c5f3ac
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-04-14 11:30:50 +0200

    Forgot to add Zinc, reduced EV limit to 508 since 510 is useless

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -231,7 +231,7 @@
 #define MAX_IV_MASK 31
 #define USE_RANDOM_IVS (MAX_PER_STAT_IVS + 1)
 #define MAX_PER_STAT_EVS 252
-#define MAX_TOTAL_EVS 510
+#define MAX_TOTAL_EVS 508
 #define EV_ITEM_RAISE_LIMIT 252
 
 // Battle move flags
