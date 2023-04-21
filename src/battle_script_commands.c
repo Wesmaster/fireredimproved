6fad539aa74560b56b6ef11ae7b697c80af1ddce
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-04-21 14:51:33 +0200

    Ball changes (#55)
    * Ultraballs no longer available
    
    * Only available balls are Pokéballs and Masterball
    
    * Pokeball catchbonus increased to 30
    
    * Increased Pokéball price to 750
    
    * Ball not lost when mon not caught
    
    * For testing purposes
    
    * Testing done, new catchbonus applied

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -809,7 +809,7 @@ static const u8 sBallCatchBonuses[] =
 {
     [ITEM_ULTRA_BALL - ITEM_ULTRA_BALL]  = 20,
     [ITEM_GREAT_BALL - ITEM_ULTRA_BALL]  = 15,
-    [ITEM_POKE_BALL - ITEM_ULTRA_BALL]   = 10,
+    [ITEM_POKE_BALL - ITEM_ULTRA_BALL]   = 30,
     [ITEM_SAFARI_BALL - ITEM_ULTRA_BALL] = 15
 };
 
@@ -9609,6 +9609,7 @@ static void Cmd_handleballthrow(void)
             {
                 gBattleCommunication[MULTISTRING_CHOOSER] = shakes;
                 gBattlescriptCurrInstr = BattleScript_ShakeBallThrow;
+                AddBagItem(gLastUsedItem, 1);
             }
         }
     }
