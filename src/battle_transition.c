lioniac/pokefirered_dev

[Fix] Multiple Quick Bugfixes

src/battle_transition.c
@@ -2351,7 +2351,7 @@ static void VBCB_BT_Phase2WhiteFadeInStripes1(void)
     SetGpuReg(REG_OFFSET_BLDCNT, sTransitionStructPtr->bldCnt);
     SetGpuReg(REG_OFFSET_WININ, sTransitionStructPtr->winIn);
     SetGpuReg(REG_OFFSET_WINOUT, sTransitionStructPtr->winOut);
-    SetGpuReg(REG_OFFSET_WIN0V, sTransitionStructPtr->win0H); // BUG: This should obviously be sTransitionStructPtr->win0V
+    SetGpuReg(REG_OFFSET_WIN0V, sTransitionStructPtr->win0V);
     if (sTransitionStructPtr->vblankDma)
         DmaCopy16(3, gScanlineEffectRegBuffers[0], gScanlineEffectRegBuffers[1], 640);
     DmaSet(0, &gScanlineEffectRegBuffers[1][160], &REG_WIN0H, ((DMA_ENABLE | DMA_START_HBLANK | DMA_REPEAT | DMA_16BIT | DMA_SRC_INC | DMA_DEST_FIXED) << 16) | 1);

Wesmaster/fireredimproved

src/battle_transition.c
@@ -2492,7 +2492,7 @@ static void VBlankCB_WhiteBarsFade(void)
     SetGpuReg(REG_OFFSET_BLDCNT, sTransitionData->bldCnt);
     SetGpuReg(REG_OFFSET_WININ, sTransitionData->winIn);
     SetGpuReg(REG_OFFSET_WINOUT, sTransitionData->winOut);
-    SetGpuReg(REG_OFFSET_WIN0V, sTransitionData->win0H); // BUG: This should obviously be sTransitionData->win0V
+    SetGpuReg(REG_OFFSET_WIN0V, sTransitionData->win0V);
     if (sTransitionData->vblankDma)
         DmaCopy16(3, gScanlineEffectRegBuffers[0], gScanlineEffectRegBuffers[1], DISPLAY_HEIGHT * 4);
     DmaSet(0, &gScanlineEffectRegBuffers[1][DISPLAY_HEIGHT], &REG_WIN0H, B_TRANS_DMA_FLAGS);
@@ -2508,7 +2508,6 @@ static void VBlankCB_WhiteBarsFade_Blend(void)
     SetGpuReg(REG_OFFSET_WIN0H, sTransitionData->win0H);
     SetGpuReg(REG_OFFSET_WIN0V, sTransitionData->win0V);
 }
-
 
 static void HBlankCB_WhiteBarsFade(void)
 {