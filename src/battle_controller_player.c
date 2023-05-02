d3b9f2a9b51dbea5c21271b60829dddddef9115d
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-05-02 12:00:13 +0200

    Forgot include

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -24,6 +24,7 @@
 #include "constants/songs.h"
 #include "constants/sound.h"
 #include "graphics.h"
+#include "list_menu.h"
 
 static void PlayerHandleGetMonData(void);
 static void PlayerHandleSetMonData(void);
