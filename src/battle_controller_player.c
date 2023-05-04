85e953aa64916db3bd4c9b9d94230e83e527e0d8
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-05-04 11:53:04 +0200

    Strange things are happening.

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -3006,7 +3006,7 @@ static void MoveSelectionDisplaySplitIcon(void){
 
 	moveInfo = (struct ChooseMoveStruct*)(&gBattleBufferA[gActiveBattler][4]);
 	icon = gBattleMoves[moveInfo->moves[gMoveSelectionCursor[gActiveBattler]]].category;
-	LoadPalette(sSplitIcons_Pal, 0x10, 0x20);
+	LoadPalette(sSplitIcons_Pal, 5 * 0x10, 0x20);
 	BlitBitmapToWindow(B_WIN_DUMMY, sSplitIcons_Gfx + 0x40 * icon, 0, 0, 16, 24);
 	PutWindowTilemap(B_WIN_DUMMY);
 	CopyWindowToVram(B_WIN_DUMMY, 3);
