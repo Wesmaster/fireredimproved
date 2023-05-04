1dc5dfa53592557256963af3c6c1e7d1a0e62cb9
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-05-04 12:59:45 +0200

    Another

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -3006,7 +3006,7 @@ static void MoveSelectionDisplaySplitIcon(void){
 
 	moveInfo = (struct ChooseMoveStruct*)(&gBattleBufferA[gActiveBattler][4]);
 	icon = gBattleMoves[moveInfo->moves[gMoveSelectionCursor[gActiveBattler]]].category;
-	LoadPalette(sSplitIcons_Pal, 10 * 0x10, 0x40);
+	LoadPalette(sSplitIcons_Pal, 10 * 0x10, 0x80);
 	BlitBitmapToWindow(B_WIN_DUMMY, sSplitIcons_Gfx + 0x40 * icon, 0, 0, 16, 24);
 	PutWindowTilemap(B_WIN_DUMMY);
 	CopyWindowToVram(B_WIN_DUMMY, 3);
