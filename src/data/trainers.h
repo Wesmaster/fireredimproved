7c1a2ac419050318fd963c1ee276617545fd26e3
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-05-08 15:33:22 +0200

    Test for double battle.

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -1018,7 +1018,7 @@ const struct Trainer gTrainers[] = {
         .trainerPic = TRAINER_PIC_BUG_CATCHER,
         .trainerName = _("RICK"),
         .items = {},
-        .doubleBattle = FALSE,
+        .doubleBattle = TRUE,
         .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE,
         .party = NO_ITEM_DEFAULT_MOVES(sParty_BugCatcherRick),
     },
