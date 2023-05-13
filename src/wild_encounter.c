452a8cc198dbc89519f3aa8b96f99d28afd43ec9
Wesmaster <34153881+Wesmaster@users.noreply.github.com>
2023-05-13 17:01:17 +0200

    Trying to get it to work.

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -83,10 +83,10 @@ void GenerateLandMonsData()
                 counter++;
                 // Kijken of meer species kan opgeven dan de huidige 12, en als minder dan de random nemen met de size van array.
                 species = sLandMonsTable[i].species[Random() % BASE_RANDOM_WILD_COUNT];
-            } while (assignedMons[species] == 1 && counter < 100);
+            } while (assignedMons[&species] == 1 && counter < 100);
 
-            sGeneratedLandMonsTable[i].species[j] = species;
-            assignedMons[species] = 1;
+            sGeneratedLandMonsTable[i].species[j] = &species;
+            assignedMons[&species] = 1;
         }
     }
 }
